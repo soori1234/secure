798569ab7c7defb422d15c872453773ec5f5e617
Soori Babu Meesala <soori1234@gmail.com>
2018-02-19 18:03:04 +0530

    Update secure-home1.js

--- js/secure-home1.js
+++ js/secure-home1.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   alert("test");
   $("input-image-submit").click(function(){
         $.ajax({
-                  url: "demo_test.txt", 
+                  url: "https://httpbin.org/get", 
                  success: function(result){
                             alert(result);
                           }
