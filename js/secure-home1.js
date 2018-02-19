c87931624353e36a6579446e29e13e2839dc4586
Soori Babu Meesala <soori1234@gmail.com>
2018-02-19 18:05:30 +0530

    Update secure-home1.js

--- js/secure-home1.js
+++ js/secure-home1.js
@@ -5,9 +5,12 @@
 $(document).ready(function(){
   $("input-image-submit").click(function(){
         $.ajax({
-                  url: "https://httpbin.org/get", 
+                  url: "https://httpbin.org/get",
+                  error: function(result){
+                            alert("error");
+                          }
                  success: function(result){
-                            alert(result);
+                            alert("result");
                           }
         });
   });
