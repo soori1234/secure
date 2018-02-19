086cc2d5c27b34341e916b717193624f6318ec23
Soori Babu Meesala <soori1234@gmail.com>
2018-02-19 18:14:10 +0530

    Update secure-home1.js

--- js/secure-home1.js
+++ js/secure-home1.js
@@ -1,4 +1,27 @@
+var inputImgString;
+function readFile() {
+  
+  if (this.files && this.files[0]) {
+    
+    var FR= new FileReader();
+    
+    FR.addEventListener("load", function(e) {
+      inputImgString=e.target.result;
+      document.getElementById("img").src = e.target.result;
+    }); 
+    
+    FR.readAsDataURL( this.files[0] );
+  }
+  
+}
 
+function submitImage(){
+  alert("test");
+alert(inputImgString);
+}
+
+document.getElementById("input-image").addEventListener("change", readFile);
+document.getElementById("input-image-submit").addEventListener("click", submitImage);
 
 
 
